5a62a99931bd58bde6ecce5dfdf2032570615250
Nimesh Srivastava <snimesh412@gmail.com>
2025-12-24 17:11:57 +0530

    compiling main using libplug

--- main.c
+++ main.c
@@ -3,11 +3,17 @@
 #include <raylib.h>
 #include "plug.h"
 
-#include <dlfnc.h>
+#include <dlfcn.h>
 
 int main(void) {
 
-    void *dlopen("lib", int flags);
+    const char *libplug_path = "./libplug.dylib";
+    void *libplug = dlopen(libplug_path, RTLD_NOW);
+
+    if(libplug == NULL) {
+        fprintf(stderr, "Error: could not load %s: %s\n", libplug_path, dlerror());
+        return 1;
+    }
     
     InitWindow(800, 600, "CONA");
 
@@ -18,6 +24,8 @@ int main(void) {
     }
 
     CloseWindow();
+
+    dlclose(libplug);
 
     return  0;
 }
